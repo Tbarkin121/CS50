20204f9e6a2105c5e883aad56c714ae74dc51c08
Tyler Barkin <tybarkin@gmail.com>
2020-10-03 18:23:40 -0400

    Moved some directories and finished the knight and knaves project

---
+++ Search/tictactoe/util.py
@@ -0,0 +1,40 @@
+class Node():
+    def __init__(self, value, action, estvalue, estaction):
+        self.value = value
+        self.action = action
+        self.estvalue = estvalue
+        self.estaction = estaction
+        
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
